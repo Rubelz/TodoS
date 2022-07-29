af6794482a0644da3f5c3903669c9f76c254c663
Rubelz <nurtizirzi@vusra.com>
2022-07-29 11:32:08 +0300

    Init

---
+++ app/src/main/java/com/rubelz/myapplication/ui/fragments/main/MainFragment.kt
@@ -0,0 +1,24 @@
+package com.rubelz.myapplication.ui.fragments.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.rubelz.myapplication.R
+
+class MainFragment : Fragment() {
+
+   private  val viewModel: MainViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+
+}
